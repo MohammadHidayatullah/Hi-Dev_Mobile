1465ae412fe2b3df50658c1941336c5ba20f553e
Moh. Syaifudin <71805620+syaiifudin@users.noreply.github.com>
2021-07-19 15:10:31 +0700

    Update URLAPI.java

--- MobileIklanku/app/src/main/java/com/example/mobileiklanku/api/helper/URLAPI.java
+++ MobileIklanku/app/src/main/java/com/example/mobileiklanku/api/helper/URLAPI.java
@@ -3,6 +3,6 @@ package com.example.mobileiklanku.api.helper;
 public class URLAPI {
 //    Untul link sesuaikan dengan url api anda
     public String getURI() {
-        return "http://192.168.1.4:8000";
+        return "http://192.168.43.200:8000";
     }
 }
